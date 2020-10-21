dbe3bb54964a2279eebd672d825d8281614ae7f1
L1rical366 <71489600+l1rical366@users.noreply.github.com>
2020-10-21 15:55:25 +0300

    Сделано и проверено первое задание

--- src/main/java/ua/step/practice/Task01.java
+++ src/main/java/ua/step/practice/Task01.java
@@ -1,5 +1,6 @@
 package ua.step.practice;
 
+
 /**
  * Заполнить массив на 15 элементов числами Фибоначчи.
  * Вывести на консоль только четные элементы массива.
@@ -11,6 +12,15 @@ public class Task01 {
     public static void main(String[] args) {
         // TODO: Пишите код здесь
 
+        int[] arr = new int[15];
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+
+        for (int i = 0; i < arr.length; i+=2) {
+            System.out.print(arr[i] + " ");
+        }
 
     }
 }
